902d45502f0861f2d44531b8a2b4f049a3c7395f
Sofistication <sordonez93@gmail.com>
2017-01-24 19:19:05 -0500

    Finish horizontal and diagonal checks
    - finished horizontal function
    - copied and adjusted code for use in diagonal function
    - both still need to be tested

--- assets/scripts/game/states.js
+++ assets/scripts/game/states.js
@@ -13,28 +13,52 @@ const checkHorizontalStates = function (gameBoard, player) {
   return (firstRow || secondRow || thirdRow);
 };
 
-const checkVerticalStates = function (gameBoard/*, player */) {
+const checkVerticalStates = function (gameBoard, player) {
   // check for vertical win states
   // vertical win states are {0,3,6}, {1,4,7}, and {2,5,8}
 
+  // cannot simply slice game board this time
+  // declare empty arrays for columns
   let firstColumn = [];
   let secondColumn = [];
   let thirdColumn = [];
 
+  // iterate over game board and fill column arrays
   for (let i = 0; i < gameBoard.length; i++) {
-    if ((gameBoard[i] % 3) === 0) {
+    if ((i % 3) === 0) {
       firstColumn.push(gameBoard[i]);
-    } else if ((gameBoard[i] % 3) === 1) {
+    } else if ((i % 3) === 1) {
       secondColumn.push(gameBoard[i]);
     } else {
       thirdColumn.push(gameBoard[i]);
     }
   }
+
+  // return true if any column is a win
+  return (firstColumn.every((e) => e === player.piece) || secondColumn.every((e) => e === player.piece) || thirdColumn.every((e) => e === player.piece));
 };
 
-const checkDiagonalStates = function (/* gameBoard, player */) {
+const checkDiagonalStates = function (gameBoard, player) {
   // check for diagonal win states
   // diagonal win states are {0,4,8} and {2,4,6}
+
+  // cannot simply slice game board this time
+  // declare empty arrays for columns
+  let firstDiagonal = [];
+  let secondDiagonal = [];
+
+  // iterate over game board and fill column arrays
+  for (let i = 0; i < gameBoard.length; i++) {
+    if (i === 0 || i === 4 || i === 8) {
+      firstDiagonal.push(gameBoard[i]);
+
+    } else if (i === 2 || i === 4 || i === 6) {
+      secondDiagonal.push(gameBoard[i]);
+    }
+  }
+
+  // return true if any column is a win
+  return (firstDiagonal.every((e) => e === player.piece) || secondDiagonal.every((e) => e === player.piece));
 };
 
 module.exports = {
