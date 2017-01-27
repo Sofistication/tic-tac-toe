b530b223ac4e816ee0c0d1b9d2dd903dc2477611
Sofistication <sordonez93@gmail.com>
2017-01-27 18:38:34 -0500

    Clear inputs in modals
    - created clearModalInput
    - the world is a beautiful place and I am no longer afraid to log in

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -1,25 +1,31 @@
 'use strict';
 
-// const utils = require('../utils.js');
+const utils = require('../utils.js');
 
 const signInSuccess = (data) => {
   $('#signInModal').modal('hide');
   console.log(data);
+  utils.clearModalInput('#sign-in');
 };
 
 const signUpSuccess = (data) => {
   $('#signUpModal').modal('hide');
   console.log(data);
+  utils.clearModalInput('#sign-up');
+
 };
 
 const signOutSuccess = (data) => {
   $('#signOutModal').modal('hide');
   console.log(data);
+  utils.clearModalInput('#sign-out');
+
 };
 
 const changePassSuccess = (data) => {
   $('#changePassModal').modal('hide');
   console.log(data);
+  utils.clearModalInput('#change-password');
 };
 
 const failure = (error) => {
--- assets/scripts/utils.js
+++ assets/scripts/utils.js
@@ -4,6 +4,11 @@ const clearInput = function (element) {
   $(element + ' > div > input').val('');
 };
 
+const clearModalInput = function (element) {
+  $(element + ' > fieldset > label > input').val('');
+};
+
 module.exports = {
   clearInput,
+  clearModalInput
 };
